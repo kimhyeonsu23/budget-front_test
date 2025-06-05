f6c118b7c2744d692a7c53d7f3916de768c3fb6f
김현수 <gimhyeonsu@gimhyeonsus-MacBook-Air.local>
2025-06-05 17:12:05 +0900

    commit

---
+++ src/components/badgeFrom.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+function badgeClasses() {
+
+    const [badge1, setBadge1] = useState(0); // 절약초보 뱃지 : 0으로 초기화
+    const [badge2, setBadge2] = useState(0); // 절약고수 뱃지 : 0
+    const [badge3, setBadge3] = useState(0); // 절약왕 뱃지 : 0
+
+    useEffect(() => {
+
+    }, [])
+
+
+
+}
